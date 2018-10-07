d6580f792809e38288b4260fe328d55a64025b7a
DanSalib <dansalib@outlook.com>
2018-10-07 14:51:12 -0700

    created first prototype with working ui and videos playing

---
+++ VRMedia/Assets/Scripts/MediaController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MediaController : MonoBehaviour {
+
+    public SimplePlayback SimplePlayback;
+
+	// Use this for initialization
+	void Start () {
+        PanelController.OnClicked += OnPanelClick;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void OnPanelClick(PanelController panel)
+    {
+        SimplePlayback.Play_Pause();
+        SimplePlayback.PlayYoutubeVideo(panel.url);
+    }
+
+    private string ParseVideoId(string url)
+    {
+        UnityEngine.Debug.Log(url.Substring(url.Length - 11, 11));
+
+        return url.Substring(url.Length - 11, 11);
+    }
+}
---
+++ VRMedia/Assets/Scripts/PanelController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelController : MonoBehaviour {
+
+    public delegate void PanelClick(PanelController panel);
+    public static event PanelClick OnClicked;
+    public string url;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPanelClick()
+    {
+        OnClicked(this);
+    }
+}
